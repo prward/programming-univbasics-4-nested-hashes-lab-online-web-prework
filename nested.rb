85422fa8361b173337e3dc6a9452ec919840d5ba
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 16:39:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     alans_new_info[:known_for] = "GUI"
+     programmer_hash[:known_for] = "GUI"
      
 end
 
