19f37c0fa3aa0cb2779d6413982bc7b49fc17b8f
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 16:38:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,7 +71,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => ["GUI"],
+          :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -81,7 +81,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     alan_kay[:known_for] = "GUI"
      
 end
 
