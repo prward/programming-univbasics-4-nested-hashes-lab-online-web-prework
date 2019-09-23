03cd6adc3db801ea51b1c5b0fe2dc7a54e6a7447
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 16:22:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,10 +17,18 @@ def alan_kay_is_known_for
 	
 	programmer_hash = 
  		{
-       
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
         :alan_kay => {
-          :known_for => "Object Orientation"
-          
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
      }
 end
 
