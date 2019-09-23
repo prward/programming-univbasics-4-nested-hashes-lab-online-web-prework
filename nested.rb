6cfb19ffa507821756ee41a52a0c0ea174eda849
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 16:21:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -23,12 +23,7 @@ def alan_kay_is_known_for
         },
         :alan_kay => {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+          
      }
 end
 
